5cb9526511b723b01fc8c1401f1e0e480bf8eda0
anjali rao <139454694+anjalirao123@users.noreply.github.com>
2024-02-04 16:22:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
-document.querySelector("form").addEventListener("submit",todoApp);
-let taskArr=[];
-function todoApp(event){
-    event.preventDefault();
-    let taskName=document.querySelector("#task").value;
-    let priority=document.querySelector("#priority").value;
-   
+let accordian = document.getElementsByClassName("FAQ-title");
 
-    let taskObj={
-        task:taskName,
-        prior:priority,
-    };
+for (let i = 0; i < accordian.length; i++) {
+  accordian[i].addEventListener("click", function () {
+    if (this.childNodes[1].classList.contains("fa-plus")) {
+      this.childNodes[1].classList.remove("fa-plus");
+      this.childNodes[1].classList.add("fa-times");
+    } else {
+      this.childNodes[1].classList.remove("fa-times");
+      this.childNodes[1].classList.add("fa-plus");
+    }
 
-    taskArr.push(taskObj);
-    displayTable(taskArr);
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
 }
-   function displayTable(taskArr){
-    document.querySelector("tbody").innerHTML="";
-    console.log(taskArr.length);
-   taskArr.forEach(function(ele){
-        let row=document.createElement("tr")
-        let col1=document.createElement("td")
-        col1.innerText=ele.task;
-        let col2=document.createElement("td")
-        col2.innerText=ele.prior;
-        // if(ele.prior=="High"){
-        //     col2.style.backgroundColor="red";
-
-        // }else{
-        //     col2.style.backgroundColor="green";
-        // }
-        let col3=document.createElement("td");
-        col3.innerText="Delete";
-        col3.addEventListener("click", function(event){
-            event.target.parentNode.remove();
-        })
-        col3.style.color="red";
-        row.append(col1,col2,col3);
-        document.querySelector("tbody").append(row);
-    })}
